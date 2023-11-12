a0439c5b92cdef7ce8cec53cf373acb78d86c8fc
todd <todd@wireservers.com>
2023-11-11 23:22:38 -0700

    Added pressable and web

--- counter-game/components/PrimaryButton.js
+++ counter-game/components/PrimaryButton.js
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
-function PrimaryButton({children}){
+function PrimaryButton({ children }) {
+  function pressHandler(){
+    console.log('pressed');
+  }
+  
   return (
-  <View>
-    <Text>{children}</Text>
-  </View>
+    <Pressable onPress={pressHandler}>
+      <View>
+        <Text>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
 
